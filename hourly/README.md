5ea76a80d54261cfcae0923bb708687811fb0a9b
Jonathon Ho <jonathonmho@gmail.com>
2024-08-23 16:28:22 +0000

    [hourly] Change folder structure

---
+++ hourly/README.md
@@ -0,0 +1,6 @@
+# Hourly
+
+## Overview
+
+Hourly is a time tracker which supports data exports to spreadsheet formats, for a simple report creation workflow.
+
---
+++ hourly/backend/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/logger"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("Error loading .env file")
+		return
+	}
+
+	app := fiber.New()
+	app.Use(logger.New(logger.Config{
+		// For more options, see the Config section
+		Format: "${pid} ${locals:requestid} ${status} - ${method} ${path}​\n",
+	}))
+
+	logFile, err := os.OpenFile(os.Getenv("LOG_PATH"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer logFile.Close()
+
+	app.Use(logger.New(logger.Config{
+		Output: logFile,
+	}))
+
+	app.Get("/", func(c fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "hello world"})
+	})
+
+	log.Fatal(app.Listen(os.Getenv("PORT")))
+}
